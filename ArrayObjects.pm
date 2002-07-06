01645f83e97f3bceee76e2d0c23a10de10dcedac
Robin Berjon <rberjon@cpan.org>
2002-07-06 14:41:35 +0000

    Import of RBERJON/Class-ArrayObjects-1.00 from CPAN.
    gitpan-cpan-distribution: Class-ArrayObjects
    gitpan-cpan-version:      1.00
    gitpan-cpan-path:         RBERJON/Class-ArrayObjects-1.00.tar.gz
    gitpan-cpan-author:       RBERJON
    gitpan-cpan-maturity:     released

--- ArrayObjects.pm
+++ ArrayObjects.pm
@@ -3,6 +3,7 @@
 # Class::ArrayObjects - Utility class for array based objects         #
 # Robin Berjon <robin@knowscape.com>                                  #
 # ------------------------------------------------------------------- #
+# 06/07/2002 - v1.00 clean up                                         #
 # 21/04/2001 - v0.04 many many documentation updates + release        #
 # 02/04/2001 - v0.03 feature upgrade in view of release               #
 # 28/01/2001 - v0.02 a few enhancements and uses                      #
@@ -16,7 +17,7 @@ use strict;
 no  strict 'refs';
 use vars qw($VERSION %packages);
 
-$VERSION = '0.04';
+$VERSION = '1.00';
 
 
 #---------------------------------------------------------------------#
@@ -128,16 +129,15 @@ nevertheless pleasantly surprised to note the opposite, it can't
 hurt).
 
 Second: memory. Memory was much more important to me as I was
-targeting a mod_perl environment, where every bit of memory tends to
-count (so long as it doesn't take too much programming time).
-Depending on how they are used, arrays use from 30% up to 65% less
-space than hashes. As a rule of thumb the more keys you have, the more
-you may save.
+targeting a mod_perl environment where every bit of memory tends to
+count. Depending on how they are used, arrays use from 30% up to 65% 
+less space than hashes. As a rule of thumb the more keys you have, the
+more you may save.
 
 It must be said though that despite the fact that I happened to be
 looking for ways to save space, it's not a reason to jump into array
 based objects and start converting every single hash you have to an
-array. Yes, I did see some of my processes lose B<3.2Mo> of unshared
+array. Yes, I did see some of my processes lose I<~3Mo> of unshared
 memory so there are definitely cases when it's useful. Such cases are
 usually when you have lots of objects and/or structures that are
 fairly similar in nature (ie have the same keys) but contain different
@@ -183,19 +183,20 @@ objects is that unlike with hashes, if your base class adds a field,
 you have to move all your fields' indices up by one. You shouldn't
 have to know such things, or even to care about it.
 
-Here, instead of using the C<define> pragma (which creates fields in
-a class), simply use the C<extend> pragma. Tell it which class to
+Here, instead of using the C<define> option (which creates fields in
+a class), simply use the C<extend> option. Tell it which class to
 extend (it needs to be already loaded, and must use
 Class::ArrayObjects to define its fields too) and which fields to add.
 Class::ArrayObjects takes care of counting from the right index in
-your subclass. The next version will also add multiple inheritance.
+your subclass. It can't do multiple inheritance, and unless someone
+hacks it in somehow I doubt it ever will.
 
 You may use the C<import> option to require that your parents' fields
 be defined in your own package too, so that you can access them. It is
 off by default so that you can use fields with the same names as those
 of your superclasse(s) (which is a plus over hash based objects) and
 also to avoid defining subs all over your package without you knowing
-about it.
+about them.
 
 It may be worth noting that the added functionality doesn't get in the
 way, and using this to define constants is just as fast as using
@@ -206,25 +207,24 @@ C<constant> or C<enum>.
 There are two ways to use Class::ArrayObjects, either to simply define
 fields to use in your own objects, or to extend fields defined in a
 superclass of your. In a wild burst of creative naming I thus spawned
-into existence two pragmata (not to be confused with those of Perl)
-named respectively B<define> and B<extend>.
+into existence two options named respectively C<define> and C<extend>.
 
-The way the two pragmata are used is the same:
+The way the two are used is the same:
 
-use Class::ArrayObjects I<pragma> => I<hashref of options>
+use Class::ArrayObjects I<option-name> => I<hashref of options>
 
-=head2 The define pragma
+=head2 The define option
 
   package Some::Class;
   use Class::ArrayObjects define => {
                                       fields  => [qw(_foo_ _bar_ BAZ)],
                                     };
 
-The C<define> pragma has only one option: C<fields>. It is an arrayref
-of strings which are the names of the fields you wish to use. They can
-be anything, so long as they are valid Perl sub names.
+C<define> has only one option: C<fields>. It is an arrayref of strings 
+which are the names of the fields you wish to use. They can be anything,
+so long as they are valid Perl sub names.
 
-=head2 The extend pragma
+=head2 The extend option
 
   package Other::Class;
   use base 'Some::Class';
@@ -234,7 +234,7 @@ be anything, so long as they are valid Perl sub names.
                                       import  => 1,
                                     };
 
-The C<extend> pragma has only three options:
+C<extend> has three options:
 
 =over 4
 
@@ -267,7 +267,7 @@ to your arrays.
                                     };
 
   my @arry = qw(zorg stuff blurp);
-  print $arry[_bar_]; # prints stuff
+  print $arry[_bar_]; # stuff
 
 Any operation you can do on arrays with numeric indices works exactly
 the same way. The only difference is that you are using names, which
@@ -288,7 +288,7 @@ You can preload this module without defining any fields as follows:
 
 In that case, C<import()> will not be called and nothing will happen
 other then the preloading of the code. As a precaution, even if it
-were called it will return immediately.
+were called it would return immediately.
 
 I do recommend that you preload all modules that are based on
 Class::ArrayObjects so that the data it stores internally about which
@@ -305,26 +305,11 @@ with stability.
 Of course, you mustn't define a field in your package with the same
 name as another sub.
 
-This version isn't really polished, and lacks a few features which I'd
-like to add in the future. Most of those features (notably multiple
-inheritance) are only a matter of a few lines of code but I needed to
-get this module out fast in order to be able to release other modules
-which I've been developing. The other features will get added
-according to the whims of user requests, my own personal needs in the
-context of my other modules, and of course whenever I may be bored
-(which unfortunately isn't anywhere near as often as I'd like to)
-
-This approach doesn't play well with polymorphism if you access fields
-directly. In general, polymorphism should really be done through
-accessors so that isn't a problem imho.
-
 As a rule of thumb, I find that this kind of class works better for
 extension subclasses than for override subclasses, but YMMV.
 
 =head1 TODO
 
- - add multiple inheritance
- - cleanup coding style
  - add an interface to allow people to mess with the internals on
  demand
  - add serialisation helpers to allow one to persist an object based
@@ -333,27 +318,23 @@ extension subclasses than for override subclasses, but YMMV.
 
 =head1 ACKNOWLEDGMENTS
 
-While I was looking for ways to reduce the memory consumption of my
-apps without losing too much speed I recalled an old TPJ article
-precisely about that. I read it, found it very interesting, and then
-the project I was working on changed so I forgot about it.
-
-Later on, I came back to reducing memory, and hacked a bare-bones
-version of this module. I'd forgotten about the article and was
-working with notes I'd taken about various ideas I had.
-
-Prior to releasing this module, I remembered the article but can't go
-check it out to acknowledge the author until tpj.com is back online
-(hopefully very soon !). I think the author mentioned having released
-a module similar to this one to CPAN, but I couldn't find anything
-there (searching for array, object, and class). If you remember who
-that was, please let me know so that his glorious name may appear in
-these small tables.
+Greg Bacon's for his article I<Perl Heresies: Building Objects Out of 
+Arrays> which I read ages ago and inspired this module, one of the 
+first I put on CPAN.
 
 =head1 AUTHOR
 
-Robin Berjon <robin@knowscape.com>
+Robin Berjon, robin@knowscape.com
 
-This module is licensed under the same terms as Perl itself.
+=head1 COPYRIGHT
+
+Copyright (c) 2000-2002 Robin Berjon. All rights reserved. This program is
+free software; you can redistribute it and/or modify it under the same terms
+as Perl itself.
+
+=head1 SEE ALSO
+
+nothing that I can think of...
 
 =cut
+
